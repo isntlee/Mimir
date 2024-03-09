339337283cfaf1ee533a136c70b98a290aa120b3
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2024-03-09 16:19:35 +0100

    isolated command functions to interesting_words.py

---
+++ apps/words/management/commands/interesting_words.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from nlp_utils.textrank import textrank
+import os
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        base_dir = "data/initial_data"
+        doc_paths = [os.path.join(base_dir, filename) for filename in os.listdir(base_dir) if filename.endswith('.txt')]
+    
+        for path in doc_paths:
+            with open(path, 'r', encoding='utf-8') as file:
+                text = file.read()
+                abstract_nouns = textrank(self, text)
+    
+                print("\n\n\n-Abstract Nouns:", "\n")
+                for noun in abstract_nouns:
+                    print(noun.text)
--- nlp_utils/textrank.py
+++ nlp_utils/textrank.py
@@ -1,6 +1,7 @@
-import spacy
+import spacy, pytextrank
 
-def textrank(self, *args, **kwargs):
+
+def textrank(self, text, *args, **kwargs):
 
     abstract_structure = (
                     'acy', 'ncy', 'ence', 'ism', 'ity', 'hood', 'ment', 'ness', 'ship', 'sion', 'phy'
@@ -8,15 +9,8 @@ def textrank(self, *args, **kwargs):
 
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("textrank")
+    doc = nlp(text)
     
-    document_paths = ["data\initial_data\doc1.txt", "data\initial_data\doc2.txt", "data\initial_data\doc3.txt", "data\initial_data\doc4.txt", "data\initial_data\doc5.txt", "data\initial_data\doc6.txt"]
-    
-    for path in document_paths:
-        with open(path, 'r', encoding='utf-8') as file:
-            text = file.read()
-            doc = nlp(text)
-            abstract_nouns = [tok for tok in doc if tok.pos_ == 'NOUN' and tok.suffix_ in abstract_structure]
+    abstract_nouns = [tok for tok in doc if tok.pos_ == 'NOUN' and tok.suffix_ in abstract_structure]
 
-            print("\n\n\n-Abstract Nouns:", "\n")
-            for noun in abstract_nouns:
-                print(noun.text)
+    return abstract_nouns
