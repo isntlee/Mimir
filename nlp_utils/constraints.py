5f18f14bd42c129eb416c36309ef3409034d8f80
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2024-03-30 16:38:20 +0100

    ConstraintStrategy added

--- nlp_utils/constraints.py
+++ nlp_utils/constraints.py
@@ -1,16 +1,42 @@
 from django.db import models
+from abc import ABC, abstractmethod
 
 
-class Constraint(models.Model):
+class ConstraintStrategy(ABC):
+    @abstractmethod
+    def apply(self, token):
+        pass
 
-    @staticmethod
-    def is_abstract_noun(token):
+class AbstractNounStrategy(ConstraintStrategy):
+    def apply(self, token):
         abstract_structure = ('acy', 'ncy', 'ence', 'ism', 'ity', 'hood', 'ment', 'ness', 'ship', 'sion', 'phy')
         return token.pos_ == 'NOUN' and token.suffix_ in abstract_structure
 
+
+CONSTRAINT_CHOICES = {
+    1: {
+        'name': 'abstract nouns',
+        'strategy': AbstractNounStrategy
+    },
+}
+
+class Constraint(models.Model):
+    def __init__(self, strategy):
+        self.strategy = strategy
+
     def __str__(self):
         return self.name
+
+    def apply(self, token):
+        return self.strategy.apply(token)
     
-    constraint_methods = {
-    1 : (is_abstract_noun, 'is_abstract_noun')
-    }
+    @staticmethod
+    def user_choice():
+        print("\nWhat interesting words to find:")
+        for key, value in CONSTRAINT_CHOICES.items():
+            print(f"{key}: {value['name']}")
+        user_choice = int(input("\nEnter the number choice here: "))
+        if user_choice in CONSTRAINT_CHOICES:
+            return CONSTRAINT_CHOICES[user_choice]['strategy']()
+        else:
+            raise ValueError("Invalid choice")
--- nlp_utils/textrank.py
+++ nlp_utils/textrank.py
@@ -2,6 +2,8 @@ import uuid, spacy, pytextrank
 from nlp_utils.constraints import Constraint
 from apps.words.models import Word
 
+
+constraint = Constraint.user_choice()
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("textrank")
 
@@ -10,13 +12,12 @@ def process_text(path, text, job_id):
     try:
         doc = nlp(text)
         words_to_save = []
-
+        
         for sent in doc.sents:
             for token in sent:
-                method_to_call = Constraint.constraint_methods.get(1)
-                if method_to_call[0](token):
+                if constraint.apply(token):
                     id = uuid.uuid4()
-                    words_to_save.append(Word(id=id, name=token, sentence=sent, document=path, job_id=job_id, constraint=method_to_call[1]))
+                    words_to_save.append(Word(id=id, name=token, sentence=sent, document=path, job_id=job_id, constraint=constraint.__class__.__name__))
 
         Word.objects.bulk_create(words_to_save)
         
